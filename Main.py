919915f23f9961f6c26a92ff2db8b733446c240e
seok jinmo <42320500+CaLicS@users.noreply.github.com>
2022-11-29 13:10:24 +0900

    Add files via upload

--- Main.py
+++ Main.py
@@ -15,7 +15,6 @@ def googleImageCrawling(keyword, limit):
       print(paths)
       
 
-
 keyword = input("검색하고 싶은 것:")
 limit = 1
 
@@ -48,6 +47,8 @@ clock = pg.time.Clock()
 img1 = pg.image.load('C:/Users/trans/Desktop/ALLFILES/Programs/Python/techno/downloads/{}/{}'.format(keyword, files[0]))
 img1 = pg.transform.scale(img1,(100,100))
 
+img2 = pg.image.load('C:/Users/trans/Desktop/ALLFILES/Programs/Python/techno/namu.jpg')
+img2 = pg.transform.scale(img2, (100, 100))
 
 pos_x = 200
 pos_y = 300
@@ -55,7 +56,8 @@ pos_y = 300
 white = (255,255,255)
 black = (0,0,0)
 def Game_Screen():
-    White=(255,255,255)            
+    White=(255,255,255)
+    ourScreen.fill(black)        
     pg.draw.line(ourScreen,White,[260,600],[980,600],5)
     pg.draw.line(ourScreen,White,[308,460],[930,460],5)
     pg.draw.line(ourScreen,White,[356,320],[882,320],5)
@@ -72,67 +74,30 @@ def Game_Screen():
 
 Game_Screen()
 
-while True:                  
+control = True
+
+mouse_pos = [0,0]
+copy_pos = [0,0]
+
+while True:                 
     for event in pg.event.get():    
         if event.type == pg.MOUSEBUTTONDOWN:
             pg.mouse.get_rel()  
             mouse_pos=pg.mouse.get_pos()
-            ourScreen.blit(img1,(mouse_pos[0], mouse_pos[1]))
+            if control:
+                Game_Screen()
+                ourScreen.blit(img1,(mouse_pos[0] -50, mouse_pos[1]-50))
+                ourScreen.blit(img2,(copy_pos[0]-50, copy_pos[1]-50))
+                copy_pos = mouse_pos
+                control = False
+            else:
+                Game_Screen()
+                ourScreen.blit(img1,(copy_pos[0]-50, copy_pos[1]-50))
+                ourScreen.blit(img2,(mouse_pos[0]-50, mouse_pos[1]-50))
+                copy_pos = mouse_pos
+                control = True
             pg.display.update()
         if event.type==pg.QUIT:
             sys.exit()
         pg.display.flip()
 
-# while True:
-#     clock.tick(60)
-#     for event in pg.event.get():
-#         if event.type == pg.QUIT:
-#             sys.exit()
-
-#     key_event = pg.key.get_pressed()
-#     if key_event[pg.K_LEFT]:
-#         pos_x -= 1
-
-#     if key_event[pygame.K_RIGHT]:
-#         pos_x += 1
-
-#     if key_event[pygame.K_UP]:
-#         pos_y -= 1
-
-#     if key_event[pygame.K_DOWN]:
-#         pos_y += 1
-
-#     screen.fill(black)
-#     screen.blit(img1,(pos_x, pos_y))
-#     pg.display.update()
-
-
-
-# from selenium import webdriver
-# from bs4 import BeautifulSoup as soups
-
-# def search_selenium(search_name, search_path, search_limit) :
-#     search_url = "https://www.google.com/search?q=" + str(search_name) + "&hl=ko&tbm=isch"
-    
-#     browser = webdriver.Chrome('c:/chromedriver.exe')
-#     browser.get(search_url)
-    
-#     image_count = len(browser.find_elements_by_tag_name("img"))
-    
-#     print("로드된 이미지 개수 : ", image_count)
-
-#     browser.implicitly_wait(2)
-
-#     for i in range( search_limit ) :
-#         image = browser.find_elements_by_tag_name("img")[i]
-#         image.screenshot("c:/Users/trans/Desktop/ALLFILES/Programs/Python/techno/" + str(i) + ".png")
-
-#     browser.close()
-
-# if __name__ == "__main__" :
-
-#     search_name = input("검색하고 싶은 키워드 : ")
-#     search_limit = int(input("원하는 이미지 수집 개수 : "))
-#     search_path = "Your Path"
-#     # search_maybe(search_name, search_limit, search_path)
-#     search_selenium(search_name, search_path, search_limit)
